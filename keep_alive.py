f9d37eccb40633e40a769c7e66dc5f30db29d74f
Иван Черторыжский <ivancertoryzskii@192.168.1.22>
2025-07-30 23:51:51 +0300

    keep_alive.py испарвелено

--- keep_alive.py
+++ keep_alive.py
@@ -1,19 +1,4 @@
-import asyncio
-import aiohttp
-import os
 
-URL = f"https://{os.getenv('WEBHOOK_HOST')}"
-
-async def keep_alive():
-    while True:
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(URL) as response:
-                    print(f"Pinged {URL}: {response.status}")
-        except Exception as e:
-            print(f"Keep-alive error: {e}")
-        await asyncio.sleep(30)
-        
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 
 start_choice_keyboard = InlineKeyboardMarkup(row_width=2)
