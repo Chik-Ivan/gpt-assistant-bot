0acef13158a5ce849416d44eaecfa0d65b12b7a0
Byter <vitazagrebelnyh@gmail.com>
2025-08-11 20:13:54 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ async def main():
         send_reminders,
         'cron',
         hour=14,
-        minute=9,
+        minute=18,
         timezone=pytz.timezone('Europe/Moscow'),
         args=(bot,)
     )
